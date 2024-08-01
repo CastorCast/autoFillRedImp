ae8d0e8fb3b67098fee80ff5db710969b5c8b1d6
CastorCast <seandehais@gmail.com>
2024-08-01 02:42:46 +0200

    Ajout de Test2, utilisant PyMuPDF, permettant de faciliter le traitement (et principalement la modification) des PDF

--- Test.py
+++ Test.py
@@ -8,5 +8,36 @@ formulaireDirectory = os.path.join(workingDirectory, 'formulaire')
 #print(type(formulaireDirectory))
 formulaireReader = pdf.PdfReader(os.path.join(formulaireDirectory, '2041-rd_4298.pdf'))
 
-print(len(formulaireReader.pages))
-print(formulaireReader.pages[0].extract_text())
+print("Page =", len(formulaireReader.pages))
+#first_page = formulaireReader.pages[0].extract_text()
+first_page = formulaireReader.pages[0]
+print(type(first_page))
+content_FP = first_page.get_contents()
+
+data = content_FP.get_data()
+#print(type(data))
+decoded_data = data.decode('utf-8')
+
+lines = decoded_data.splitlines()
+#print(type(lines[0]))
+lines[0] = "Tomate !!!"
+output = ""
+for line in lines :
+    output += line + '\n'
+
+encoded_data = output.encode('utf-8')
+if content_FP.decoded_self is not None :
+    content_FP.decoded_self.set_data(encoded_data)
+else :
+    content_FP.set_data(encoded_data)
+#first_page.get_contents().set_data(encoded_data)
+
+first_page["/Contents"] = content_FP.decoded_self
+#print(type(first_page))
+
+#print(first_page["/Contents"])
+
+writer = pdf.PdfWriter()
+#writer.add_page(first_page)
+
+#writer.write('Output.pdf')
